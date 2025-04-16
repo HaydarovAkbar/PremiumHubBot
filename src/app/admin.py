37a822866fc0f3ffbc73f110671ba833612c2d41
KhaydarovAkbar <khaydarov3255@gmail.com>
2025-04-16 15:38:30 +0500

    sd

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import CustomUser, Channel
+from .models import CustomUser, Channel, Settings
 
 admin.site.register(CustomUser)
 admin.site.register(Channel)
+admin.site.register(Settings)
---
+++ test.py
@@ -0,0 +1,4 @@
+a = "fe1e95eb2d81c8b2ff03cf7b8f5bf391ddbc248c136e7b6ac9d3ab2b6f973c26"
+print(len(a))
+a_b = a.split()
+print(len(a_b))
