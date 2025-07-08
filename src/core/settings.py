0beedca6096c78ddf0791b8f7679ade1d3476ea6
KhaydarovAkbar <khaydarov3255@gmail.com>
2025-07-08 14:56:06 +0500

    fg

--- src/core/settings.py
+++ src/core/settings.py
@@ -116,7 +116,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-HOST = 'https://be49-213-230-69-88.ngrok-free.app'
+HOST = 'https://5de9988f26b0.ngrok-free.app'
 USERNAME = 'AiGeneratedPhotoBot'
 ADMIN_USERNAME = 'premium_xizmatim'
 TOKEN = config('TOKEN')
